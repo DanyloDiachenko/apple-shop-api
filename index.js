a7ab96fead581fd0574d8b320bcbfb1937eabb49
Danylo Diachenko <danildiachenko23@gmail.com>
2023-02-23 16:28:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const macbooksRouter = require('./routes/macbooks');
 const indexRouter = require('./routes/index');
+const macbooks = require('./data/short/macbooks');
 const app = express();
 
 app.listen(5000, () => { console.log('Server started on port: 5000') });
 
-app.use('/', indexRouter);
+app.get('/short-macbooks', (req, res) => {
+    res.json(macbooks);
+});
 app.use('/macbooks', macbooksRouter);
 
