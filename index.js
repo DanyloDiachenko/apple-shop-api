5052ef1de8c81d7cf4469916cc62f02fabf6f00b
Danylo Diachenko <danildiachenko23@gmail.com>
2023-02-23 16:36:04 +0200

    _

--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 const express = require('express');
 const macbooksRouter = require('./routes/macbooks');
 const indexRouter = require('./routes/index');
-const macbooks = require('./data/short/macbooks');
 const app = express();
+const favicon = require("serve-favicon");
 
 app.listen(5000, () => { console.log('Server started on port: 5000') });
 
-app.get('/short-macbooks', (req, res) => {
-    res.json(macbooks);
-});
+app.use('/', indexRouter);
 app.use('/macbooks', macbooksRouter);
 
