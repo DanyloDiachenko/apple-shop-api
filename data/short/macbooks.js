97f42d368ed566c888518456dab0fa1e2b810368
Danylo Diachenko <danildiachenko23@gmail.com>
2023-02-24 22:42:50 +0200

    Update macbooks.js

--- data/short/macbooks.js
+++ data/short/macbooks.js
@@ -6,7 +6,7 @@ module.exports = [
         src: 'https://lh3.googleusercontent.com/8hlQZyQdy_MrtvLSZ0Dtxp93yJ4AJnLk5pbMwYasgf8OC2mwgkooP7vL0upge9t_WiTGis_rKqKjXtt1gETOPSaYVt83XW0lbHJfDppPpUzICjhnalbcgsq4_tlyDqOYe7h-dL47khsHABOOxxBZ5RXDYRIBayvT0uz0dnRLaO25tquhv1SQZYj-dcQEVxWZqUGywYsl69JOYg2AQIu0402bpdCi5hC1LenKHA5GCjJ3MzdIGQ4FxblyanU0mUzgjLUiS_io3CSf_zq3laVjVwfbczOMmMzsyp887nCfBQv7Y9Xj4AQZiXJxO9FaIkcRg7fEXJ5ZXLsicnPZFjnCw7XIxLHji_Z6JKylbTwiIwOVAIQ5uBVKI9_2Vd938e1cqavtZQBeI0hEjzUKJm9ZzZ1UND0SmRHGVvsTPiwJyFEdDMmuCxzJ77lzWuTpY5Sxf12JHNYqgfZXNAA15FYBwDfa38sn5lkKMji05__B2EGl-XxYX4QwBu-v8mHXNQ1CXNfB6nQi57Po_8_qKBa8sTDKqwjaVp2Ky1iwDwQQ3bOGS5AOUeQoRC9fGVtvCCZ6yJbrNr6ooM2MWwJZ-92QQ9WfMj0gAk3WDxJjKBchqErrUHW54hRnUb-tGTskE-7QkZmiYEElmTo94M2PepXSpo-WSw1IaYYEC5GtXX4P_EKgZumz6-mj0_RugAObJVx6VxBuIAVIcg5xoS_j0dwzHFQ190umHSPo9xrCZtz1UG2kz8fKfe6RRtx1LpKLx9tmIJyvDooCEI9Sv_GM5nqcaLijIN8F6MV4APMLpAFsjb201wUqdaXwFWIglB9ikIkf0qx-g7F3i37YcuXXN-iu-fFMquZDoZA6HRgq54M1S50zaglWQ587i1DyLUwiiq3BsDOtrqrAmsXF7DXM9Expbf30j7SyRibcbW7Q3NWRJKbvBbRs=w368-h223-no?authuser=0',
         isNovelty: true,
         isDiscount: true,
-        rate: 4,
+        rate: 5,
         price: 6399,
         prevPrice: 6500,
         kharacteristics: [
@@ -19,7 +19,7 @@ module.exports = [
                 value: '13.3”',
             },
             {
-                key: 'Разрешение дисплея:',
+                key: 'Разрешение дисплея',
                 value: "3024×1964",
             },
             {
@@ -44,8 +44,8 @@ module.exports = [
         id: 2,
         title: 'MacBook Pro 16” M1 Max 8TB Space Gray 2021',
         /* description: 'Суперсила специалистов. Самый мощный MacBook Pro в истории. И это монстр. Супер быстрые M1 Pro и M1 Max – первые чипы Apple, разработанные специально для профессионалов.', */
-        src: 'https://lh3.googleusercontent.com/2JCu4cF7Jo32SwivzEU6SrDjfXBvy0dHBLdXx2uuqprUakOzeBbr6Ms_w-TTrxKAtQHldOfoMiggC9o_Dn-FTd9rHifUEcPMDmte1VrTxSkznOTFnHrBOClNgn3UaaJarN0RyRfusRHcr184Cg2HdncNRwM1pNp2ao5Zo8Y1WjddJWm-O0znJHJ9xiPkqMLFN0AGwqtbNXGQM-WNfZi70ZlB4Wqx_DwXh4OIFeeN35V1jKqJqLX54s7c67HhxeLd7gN-XjJoLezjpy-f1YwiqdYHDYSUjkCbRWIXWuaL2VRtjQ6TRU4lBH0tERUvRO3Dxf_RK4edHilI7BRXErzbuBVuoTKdItt7HO9bPt-AhZ3GtRdry9kF12KOq8XguiwfSDNasgj_c15IrXC9wh2Km6ttrtN2oUFiMQBry3XxbSaAVQD5Kc7Cl9S2GcSEf6eM0ZFbfbKM8okZmbRaxPkkeHK4xbqw8Ynyy7xGMWu2MMIbs9mxBaGgmpMIBDuOFh4KsW3lvwqu31V-C7mAytb26zn7R9g4ZtDqf8O0R2LMw85kFtV5PV1yfyUwdJom2AV4dT6W6agZW_tCOg3gtiHkNmP3TB7wFbqdk1G11usfvUYYafA_TBdWJsbxP_9enOJ5yolf17FYNaqVEg1YkUN8H4Tp8cKIY4mj08pa722x22JmsHF1szgtpASSLSetU5xaUuW3VQkL2soABpesiy-kB2zOY1cd7MEZIT2oNnp4rZOAcAodvrgHRwIFnWKex6Do-rSxRo5yuat89OugU-ZDR9qbW-ApXnKSNwR5DTBRovYgFjJeA5OW9dBW1npHy-1iViivwBMSOACiHbQlFMeY-sDOnTT-N_VjaBWPjzldWSKPVpB2K7X2-KYWEaTS_olLHuu4JKUrZBC5fxHNzmDWdm40fJy-q9SVHGuYdpdFBLo08Czd=w342-h210-no?authuser=0',
-        isNovelty: false,
+        src: 'https://lh3.googleusercontent.com/uoy4Q5Ce0XDQpq9v7mS65o_E55tjjYxXEYKMF5EwCOodNt0o7bSNAb7j_riao6hoU-dWz9DxD7Pnjb0latRUKI6olAxtuExieYBoOuCG6TbJwyRHCLYBKWifIWhXzTuZ3eUzB8V333p2aT7ES2H-WCW4MsxL2RBiz9kw5hFLUM6JiT6N0ydrSsh07YFOspmOl9rc9i3d6iFlp7cxi0yvTv0VkCr6HafBKuG_uTTlNSitlB9yTSf68M0B3MC4Gd4ZjlvT2E2MppIX2_u1kluzkUTXG-KhF5ZlxUg5fo_0VEvt6AnRhgeYctzH3tjd19xgc0gL_3LPm7740uUiIt4RBRGFf_Z3BzCvUUNFQOHd3kdY99uBgKP6MGOzA55tU100XeF-1XkVtl1Mz5deaViuxhpk4aQZcjfs8OymxLg3kSeb13zBo9xnp7P1Q-KhEU3N6phf1WpcFXcJ-WjQVeaPYOmxYjyXSdl4LnWIPCmtw7nRf4vLhITV7Xga4s641pM604VBfHJjet4BpLOmiji_imCOISqggLcS3eP7hFiZHn1i6frnmt4Q1W4o3EVmGWoxkJnivEi9J5OQaxs7Vt-zrAUpwB5tE8gwuBh7FDvyW3aDgIaCEeG_UW5-HK48ANHA-TIIYfgndHEZGoMRQH6-X-cA3vKuK2-KoMOshZKNYrP4DSlUogTeKhhSVie0YYu6N9arUbZwvEhtq0enu5laihwYHWgAZPh1uQs-0rT4tMDGwmVvGIqPmLgbvNGlCSNALNiCvPgcNP56NW0NsOJf9R093RVezBBBH1FcdXiVYANG68I-x6FyajU_NRHbp6WfQ7fmvlFn8k0nXaLH9oVfqxA2FRbnYHzLhzyS2OzSnfSTXw2pRuXhsqNoqoiz--E11HAlT8A8N7AvvR0kq1g8G46XWxDJ-yApsVahe8bTXsKEaduB=w720-h440-no?authuser=0',
+        isNovelty: true,
         isDiscount: true,
         rate: 5,
         price: 6639,
@@ -60,7 +60,7 @@ module.exports = [
                 value: '16.2”',
             },
             {
-                key: 'Разрешение дисплея:',
+                key: 'Разрешение дисплея',
                 value: '3456×2234',
             },
             {
@@ -101,7 +101,7 @@ module.exports = [
                 value: '13.3”',
             },
             {
-                key: 'Разрешение дисплея:',
+                key: 'Разрешение дисплея',
                 value: '2560×1600',
             },
             {
@@ -119,6 +119,252 @@ module.exports = [
             {
                 key: 'Цвет корпуса',
                 value: 'Space Gray',
+            },
+        ],
+    },
+    {
+        id: 4,
+        title: 'MacBook Air 13” M2 2TB Silver 2022',
+        /* description: 'Новый, легкий, суперпроизводительный. Новый MacBook Air работает без подзарядки дольше, совсем не шумит, потому что у него нет вентилятора. Мощность стала еще компактней! Кастомная модель! */
+        src: 'https://lh3.googleusercontent.com/RP74jU0QE_6ItPChiAT0R9UJ-EkVdP2OcZaUS4Z6c9SHyWOFDrat6YWEL2D4CA6ZkJDp2Prf9-malIoswRHiCAc4_XnwzEJHz3n2MBdaLQpdd7qzZVnAhmr6W5ZjAn1T4f-5GZO-zlXfwZ_KLCbb7PVwn5nPYHv2mqjMMNXfTpHbHRahkh3bjSDUBx51XCe8q3pYyNt42JeLgCHvXvMKXyTkryq7ij4d2e9EFKUUhchTExTfnJf-adA0O5qwgcz7Q030rehsoB0HpPJ7aDRc785My2axbrxQldxXcENg2tqsXru8Ug7Q3Q-zUNLUsZuHJOr85ncxTnhIm9VaaOoWqBJzEaHNWScyo-yMh8vpWR42QFvzVMLBascn4bniS2gAREPYwF4iFDfsxjZ921T8pjRWPxVSZjYtYTJBJFlJNRYYTfQWCtmRwhpt_CVlWK6WFjbRyw9eOJllUDOHqcGVn4joolJ6ycnsMatAsgeWG6LUOWTJb_SIbgqW9QBWJCfrWWJHBBIxTgXywUCLZVE331k_OB0lurDn_eFroAPJZVTuUppS7yV_WokT-e_sxQm_wuo-H6NLGsbznGeDzGwKEHDx6whk37WUVSi2fl0K6lcmpm-JrTW4rLrm9CYw7PKnIPwNi-gk34wf2kVQtb41AZL9EWCbSkSsdxSvCHfxivlyUOHss1tXZhiACNUQO9x1-Tol1fKuALivNKj9PUh6Qg4fTRutkRm6rFsA9iJtquWFwq3Ev3HocQnxkelaCCmc9ETFBaZWAE9rjtGlI5kdrt7IlOYBfWOreeJDslJi5hNGPxoFCQX8h0MUoSSVHoHvBY4_MK7kbbuY91ZHsVgdikNpW6mZNmQ-YLgJhXetra03u1jLHK1ecABMHB06R1F7Y0aEmCMW701a6sJKkVFg09x6rlEfjDCWEwUamevTgTGzWXfp=w436-h263-no?authuser=0',
+        isNovelty: true,
+        isDiscount: true,
+        rate: 5,
+        price: 2799,
+        prevPrice: 2900,
+        kharacteristics: [
+            {
+                key: 'Год выпуска',
+                value: 2022,
+            },
+            {
+                key: 'Дигональ экрана',
+                value: '13.3”',
+            },
+            {
+                key: 'Разрешение дисплея',
+                value: '2560×1664',
+            },
+            {
+                key: 'Процессор',
+                value: 'Apple M2',
+            },
+            {
+                key: 'ОЗУ',
+                value: '16GB',
+            },
+            {
+                key: 'Накопитель',
+                value: '2TB',
+            },
+            {
+                key: 'Цвет корпуса',
+                value: 'Silver',
+            },
+        ],
+    },
+    {
+        id: 5,
+        title: 'MacBook Air 13” M1 1TB Silver 2020',
+        /* description: 'С новой силой. Новый MacBook Air работает без подзарядки дольше, чем предыдущие модели. И совсем не шумит, потому что у него нет вентилятора. Мощность ещё никогда не была такой компактной. Кастомная модель! */
+        src: 'https://lh3.googleusercontent.com/1W59I9tHlIjbYzVEzN3Qqg6DMbzZmShihZahL4GhOohe_4T755Iy7TVfhiMpnmaec7Qzll1eyZppZHIkvrrMZPpXxgi4jX3Zu-wGzxJNpIAco6uRs6wudnBNrAxApW8JHW5MDqCJhRG1bd4VDYeZEqHG6YodipBLUuBRYTCcvr7oJ0Y-2YcisonEbf60attd9AtvK40cdncA-CaI1VcdrAuUkJtwpg0KZNnCUmWjsj1S7i_8H6qCONU-cYHRewuKiY_G8aCHOe0HUR96k0MJF2oOSI258AVAu5GoGXc_E2C0FbipYu7SiwBP0iA17dripOnAQU4we1eeKDgVUpa5FZeTcRSxWlaANADnkqui0ND8_yOtGA_qVrL1QZspDHOH1vbY7ImA7hxbvFLAp0bToAUk_sPzVr1uZZRClBtQgt-7Auwp9VQfqFZVpjbV3nsKn58CRpukLL7MZQzjzRcLQegRDLSZvrWOVjoe3tJMjjvJewgdRoXUnoHpfxrRq-OLwNYYuRstbvVLPCH323k8yipjmglg6GZ1OHVwCVO3lG0i7E4EKJ9RCev2LBWvRlccj2T0OHZjmSawBSUX5mMl87v2nkDFBOVDjYBqbKsMpQz1z_v-GNEqEMDnywLqlZwOAK3hEzWWYmnNtevwBCtzySO3rPBSHJsv3iOh33zYlk2TvSzy9Aa8OomIbHd4t5co4JZftG6CRM-prEbZfu_jT2MlaMNRShUjF6x11kusNHZir1W-C3PEDEzHQyONW8JRzJT4yE5qT6niHnhvJfCk7PApEwCZ0bJBi0RpJngLI5NGfFowbFOmQnqN4UUXkgOsIRBkdL4cpz6ytrZfIRT1FwjR2DF3VE9eWqCa9CRUCMs9WPNZgyW84DuJdSpEUS4qE8RCM42P-TVmOae8g8DdpRiMiRMYo2lkNtxt1oqBcd4HfCzX=w500-h284-no?authuser=0',
+        isNovelty: true,
+        isDiscount: true,
+        rate: 5,
+        price: 1899,
+        prevPrice: 2000,
+        kharacteristics: [
+            {
+                key: 'Год выпуска',
+                value: 2020,
+            },
+            {
+                key: 'Дигональ экрана',
+                value: '13.3”',
+            },
+            {
+                key: 'Разрешение дисплея',
+                value: '2560×1600',
+            },
+            {
+                key: 'Процессор',
+                value: 'Apple M1',
+            },
+            {
+                key: 'ОЗУ',
+                value: '16GB',
+            },
+            {
+                key: 'Накопитель',
+                value: '1TB',
+            },
+            {
+                key: 'Цвет корпуса',
+                value: 'Space Gray',
+            },
+        ],
+    },
+    {
+        id: 6,
+        title: 'MacBook Air 13.6” M2 512GB Space Gray 2022',
+        /* description: 'С новой силой. Новый MacBook Air работает без подзарядки дольше, чем предыдущие модели. И совсем не шумит, потому что у него нет вентилятора. Мощность ещё никогда не была такой компактной. Кастомная модель! */
+        src: 'https://lh3.googleusercontent.com/7Vp5yhwfU1IdfQEpwn3K4hubsoYR7lzEPB6u26BgZV0Db10gH0yP3az-rv7dL4Un95qGE_dOgcLx5DPncthPdJh-8De-GaSz-_8BUWuqjL71dcPgmGsFtwSNrd0VHs2nfUoS7XwmJRIby5nLOMyieZt3A5KMr6sC0OAsQ1gZmoh8PQFfMt_wGLfwhz4VC6N1H9EW_adxOFzY234nAjCLnzzjyBSI4Gi7cQklanRUppAGj5HubiojkT-9WpBrRLvXLs71Eyn5cr3QtztnLRR5Gwg5ZEd2KkNu0--Ds2KmTZ9IWOQ31vIAIja5AQm-mjn6sMWXZq8CaQW-49VlkgIaJVqDmBIZWTwgorQus7LBe4Zi-MgaR_ZTIe9s4SJzOi9GvYFzGkem2VxtnCbz-o37gcZEuzeQuCb4S02itUUlB-RkZJE4rln7aqHGGvhfhqrcUKIjukEgHb1NRgsEfPCOqwveQ5seDPrUU3ZkMlDiG3YtDwOGLHVvKVmRrLLtxL6EPPq6x8kcu_UTXUABjTCl3AVx56-IMcNwJFQALtNPrBpg00btPhr4fV8CG4r0Bbk8tCrPB7yB-EFEpoVOEo4wekmL230zZ_lx0KDDuvRHD28Ub47oOF4AWYY2HPx117pkPKm-Yk0B2UTi4bl5rRSFajyjUabwTQDglEGi4v4b9lwggEQX8czxQ8wNBnDat-Q0IKFwGUpVuoHgQh1sss3be7HwTSVkGxtIhYxeR3BRJRiXmjrZDCKYZJ5dN_oOHCEO2VED0dMi3VTaHbwIV6J3Y72uNW2l9h4fTHEO8lUxFpMdPKA8fVSKxwwS06KAu5x4mXwAKHsiRruy2EqxY9hBhpHmtzcUqTQwF22Rcl9tf8Vw1gHarPLFYee3R_BUofFKRtwpR5qzZL54Xbl7vsZjRUq6R9S5DSL5xxLnd213zWNiofvJ=w366-h221-no?authuser=0',
+        isNovelty: true,
+        isDiscount: true,
+        rate: 5,
+        price: 1999,
+        prevPrice: 2050,
+        kharacteristics: [
+            {
+                key: 'Год выпуска',
+                value: 2022,
+            },
+            {
+                key: 'Дигональ экрана',
+                value: '13.6”',
+            },
+            {
+                key: 'Разрешение дисплея',
+                value: '2560×1664',
+            },
+            {
+                key: 'Процессор',
+                value: 'Apple M2',
+            },
+            {
+                key: 'ОЗУ',
+                value: '8GB',
+            },
+            {
+                key: 'Накопитель',
+                value: '512GB',
+            },
+            {
+                key: 'Цвет корпуса',
+                value: 'Space Gray',
+            },
+        ],
+    },
+    {
+        id: 7,
+        title: 'MacBook Pro 16” M1 Max 4TB Silver 2021',
+        /* description: 'Суперсила специалистов. Самый мощный MacBook Pro в истории. И это монстр. Супер быстрые M1 Pro и M1 Max – первые чипы Apple, разработанные специально для профессионалов.', */
+        src: 'https://lh3.googleusercontent.com/LesVOKAqw7qO2E3XzqGvOp35ElMIY8aR0DZK5-Ofmyo-U1eL3PTg_8Qm9ZIWg6kMNB9PyZLp6y5_WMh4usPhwvEdKTvLwpU6SZSAFoxfx5uHcUeI4Z7e4nCeaKnwJdajozbM_IdCHLpqPrYlvrcW0Rqw6zXEVeFdUUpZsQ4Z3VHJHTVvZ0Ox6YQlEflLhQ6F35UCAz2j6cm6l2F_lo5tFGkrrE97OhSZW_9OrKMqutOCAvD08bEOf64uu8FPubZ7iHR8PT8avzkcNQEi_S-tfLziVSRcuudgecWuICZAJOoCS-vXD79ZshS2dO11-6uqv_8nQ8Xpdo7McmiBDvLFPv1gPADhDaf3aaqsDSyxr45OgY9-1pFRn5vUgQ7aQg0Gzbh4Vsxxy2ZcDqxgBwp41_HoveqnlA0oEF2n2n-Opo9lT1jl9nY1-VrNtXCeEIrUS2tZZB9vAY98Gwh7dUQ2euO7ofWx_s3m1-QtYY0PUSwjmIHAFyMDtW9x4CjmK3U4YeZkuPuM1VpTaCryKOgInIJPHaBnMa4gezyKYkJuI4Ze13XS1X0iLoYS4apVGumh_tizJ4dCwFViZU6uoqQxc-U5PDPjUXE2QO7W-Z2-5wSL6GmWR29jNdXfjuEerZQsuBWYQy_bROql-iXOsL0LrCfwff-xHuiPxYI6_mgozy2lHB-CHEibbQJUad45hn-H61L1KiC4-ri4_9QgphHsoxHofRUIFV7wcOVj_tHJQnneKPSVwM1uHwaKW5JqO0Rn9Mdy8HTc2xcXqry-XNWX-FNItnSrGu7d7hsHCHoPqjJhLICRHtp23Jc0hnpXShuZyzW4HKw4ry8F9nFsVXNUGSVSwDj_tPTQEYwsrMg5ieICPcOEjxkkKsYyxOahNmMNu3mfAfqPFHGXM16aYned7hG6b-8wwNgfgM_4htFyqJNXVsYo=w720-h440-no?authuser=0',
+        isNovelty: true,
+        isDiscount: true,
+        rate: 5,
+        price: 5499,
+        prevPrice: 6000,
+        kharacteristics: [
+            {
+                key: 'Год выпуска',
+                value: 2021,
+            },
+            {
+                key: 'Дигональ экрана',
+                value: '16.2”',
+            },
+            {
+                key: 'Разрешение дисплея',
+                value: '3456×2234',
+            },
+            {
+                key: 'Процессор',
+                value: 'Apple M1 Max',
+            },
+            {
+                key: 'ОЗУ',
+                value: '64GB',
+            },
+            {
+                key: 'Накопитель',
+                value: '4TB',
+            },
+            {
+                key: 'Цвет корпуса',
+                value: 'Silver',
+            },
+        ],
+    },
+    {
+        id: 8,
+        title: 'MacBook Pro 16” M2 Max 1TB Silver 2023',
+        /* description: 'MacBook Pro, оснащенный процессором M2 Pro или M2 Max, стал еще более мощным и эффективным, чем когда-либо. Он обеспечивает исключительную производительность независимо от того, подключен ли он к сети или нет, и теперь он еще больше времени автономной работы.', */
+        src: 'https://lh3.googleusercontent.com/z6uNv9xD_0v_wHuOJWjXFcvsrOg48pTxLohS0ftnqaOW6zt50O8e7iJgzW1qA5AqpqGgUVu6oanj6COpvFg3bJmsBujJEncr-PYwsWI4r1for1_4h8DE9DGetwgdUHlMVIWHAvBl5IFxfjmw2FZuk2hDvLT_TuO9CphP1_9kYrTLA1rue4f7bZlnfaNsKbjr3woDzGIQV8V-Cy4a3Z0P3mX81Y2A_PGoYOuWECgStRIaMZB5YSHRpw6TXqY34Ha0aFVCWwR1uew-fxQzlS9yD8dQ52xA07cFbwarK1Gpm6GPZJtENhMuJaajtvoGtnbqSBl-roXHpYb__MPjeutdkGS3VYZp1lb958m9rPoCHxkWsZA68-hQO2pZVocYji73m3mD2QI1lEAsduqqStL83IJ22cyroH7CtEtmjcsVRT-KThaSGlSJB7xy-V1NaaZtOiV4V2OAIWl-UgGQbIoPoGbDGmpgSIPr39gcNQHb51S6gQCWgxaR_n0x_nhzuJk3uPbYwbBuX-Z-ALZcHT-3u2eZNdyvYg_CzR2hLDxSAFjXRjntEvUVQ4qgoI_TvQHxJJjcjN6jlGFwYVJBscnbJ1XZkbf114FmYb4Lzlx2G5JzrrtNZXPTAohQT_lAZ8Qozjgggvw5P13ga-AU76_SsdJZbozirdPtqGG23C9g7np1Qt7SUY6AwiGJmZxGFycjSl6Mg_1iMsXdmdqGQNozuz6jiIkUrMlZj5rQMtbrQSTtLie-FOHbwgNAo_u9Gmm3YT4fZ0TBSss0s-CZCF_f7upwNC-yTELBv-ftkssykCTyc5lI8mhOdKaJHRDaOSHNzyLAVoHTq5CqlxfuqEYtHfnBoiwPcFj07cEmYVhv-YlKuu9oJ8Sk0aMuhoKRt9ndMq-mcTrbUWM0SdDW8mWl3ADSrIWTjuKlvAK1w5QMM6B50ixL=w720-h440-no?authuser=0',
+        isNovelty: true,
+        isDiscount: true,
+        rate: 5,
+        price: 5999,
+        prevPrice: 6200,
+        kharacteristics: [
+            {
+                key: 'Год выпуска',
+                value: 2023,
+            },
+            {
+                key: 'Дигональ экрана',
+                value: '16.2”',
+            },
+            {
+                key: 'Разрешение дисплея',
+                value: "3456×2234",
+            },
+            {
+                key: 'Процессор',
+                value: 'Apple M2 Max',
+            },
+            {
+                key: 'ОЗУ',
+                value: '32GB',
+            },
+            {
+                key: 'Накопитель',
+                value: '1TB',
+            },
+            {
+                key: 'Цвет корпуса',
+                value: 'Silver',
+            },
+        ],
+    },
+    {
+        id: 9,
+        title: 'MacBook Air 13” M2 256GB Starlight 2022',
+        /* description: 'MacBook Pro, оснащенный процессором M2 Pro или M2 Max, стал еще более мощным и эффективным, чем когда-либо. Он обеспечивает исключительную производительность независимо от того, подключен ли он к сети или нет, и теперь он еще больше времени автономной работы.', */
+        src: 'https://lh3.googleusercontent.com/E5Cm9zJFLd9YdbSRiKRMJoUaG8KP0Vhhh5gFnVWIPaX62xacQh1i8z-cVngSjBeBz08ZY74Y7k0YLe6TOrKE9ujLMrxL1WnUIvMhtD3VlwZDlS78N786SlUh1IfQ9oIwidn2uobIanpTLhE3lPgdDAhTzYju398R26sAnHmxKvdlbP3sSG8fMs1bOb1_li3G0PSPvDVW7MAjGnl84rs_7JgpQN3kteGSC3V1yw1B1qE7ky_Ijl1b9KmIgb0zYF1NzLYX56JU9qm_0Sdk5sRThD6kU02VpjKenRkuHr4ZnGwHT1Vmy49K2nNonG-ChOkguszFsoCFx19r4qecv38-EF3eYcucKc67zy3XhfyPQXqdNFKYOmFeP88UwXU2OAFSr7gz7r0VVmtymoN1kvVEjL7iRNlYh9orA-l9VAvH6F_tLa7wyXvEEFm6ylDfv5R131NkOrppDlM_bmBvD2WqbqadA1Vna20YQEk3-wcZTvsoBdnpB0eH6q_rQ3BXmFiMbOtJ-pe8ys8IblGApX0Me1COoYFMG-ncEe43PUmPgaLTAaWDTs72sGbbetmG_V_qOrcBmGniKiL_C3dkZlmKyFD3g6Qqa10FLLRafbLHfiyWSwL1Ot3POIRhbRO0eFITrbe8yEc36zhmQx7U1n6GHTs7JEjOhc8lC8XUHy0SKgZ3eLXBbOyGJP6cCdtqD6zONdIJs_x6tyEwCatiDZODj7Dc6k1KeqGgTuNzUt1SkBeUfpGrC-AF0v61jCj7c8e0z-SHVKYGedEsuxzytj961HDbPUnnFlnO05WhEVOjXhgBpiJKdUar8dFLrYEIke5YipV-NLEhvWTrDefRyapomzWKpC9m37SsB4W0ruCF5AbWICXAL56tXQGhi23e994pmK_4w_R8_eG-3oGen0rX7MmVpcKrpPHCTURpprgOJlkMMJP6=w376-h228-no?authuser=0',
+        isNovelty: true,
+        isDiscount: true,
+        rate: 5,
+        price: 1599,
+        prevPrice: 1700,
+        kharacteristics: [
+            {
+                key: 'Год выпуска',
+                value: 2022,
+            },
+            {
+                key: 'Дигональ экрана',
+                value: '13.6”',
+            },
+            {
+                key: 'Разрешение дисплея',
+                value: "2560×1664",
+            },
+            {
+                key: 'Процессор',
+                value: 'Apple M2',
+            },
+            {
+                key: 'ОЗУ',
+                value: '8GB',
+            },
+            {
+                key: 'Накопитель',
+                value: '256GB',
+            },
+            {
+                key: 'Цвет корпуса',
+                value: 'Starlight',
             },
         ],
     },
