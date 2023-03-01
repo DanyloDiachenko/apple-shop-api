6a8f1a518664cbe4f7b65b8e74068962ff151cef
Danylo Diachenko <danildiachenko23@gmail.com>
2023-03-01 19:02:19 +0200

    responce

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const macbooks = require('../data/short/macbooks');
+const iphones = require('../data/short/iphones');
 
 router.get('/short-macbooks', (req, res) => {
     res.json(macbooks);
 });
-/* router.get('/short-iphones', (req, res) => {
-
+router.get('/short-iphones', (req, res) => {
+    res.json(iphones);
 });
-router.get('/short-ipads', (req, res) => {
+/* router.get('/short-ipads', (req, res) => {
 
 }); */
 
